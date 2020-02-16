95a6b11e829eee93405358719460f7c1b6f51964
jciria74 <47560520+jciria74@users.noreply.github.com>
2020-02-16 23:16:22 +0100

    subiendo componente receta final para usuario

---
+++ src/components/FinalRecipe.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './FinalRecipe.scss';
+
+const FinalRecipe = () => {
+
+    return (
+        <div>
+
+
+
+           
+
+
+
+        </div>
+    )
+
+}
+
+export default FinalRecipe
