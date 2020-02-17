7ec2944e68958bb37f81c5d477718e5873342059
solar <jarabelmonte@gmail.com>
2020-02-17 17:07:58 +0100

    routes install y navbar footer

--- src/App.js
+++ src/App.js
@@ -7,10 +7,54 @@ import Title from './components/Title';
 import ModalHungry from './components/ModalHungry';
 import NavbarF from './components/NavbarF';
 import {Button,Modal,Container,Card,Navbar} from 'reactstrap';
-import Contenedor from "./components/Contenedor";
 import NavbarFBottom from './components/NavbarFBottom';
 
+/*
+  state = {
+    recipeRandom: [],
+    isLoaded: false
+  }
 
+  
+  componentDidMount(){
+    (fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=1&tags=vegetarian%252Cdessert", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        "x-rapidapi-key": "9f3dcab0dcmsh07fe862641b36d7p1dd96ejsn9d48b10f1174"
+      }
+    }))
+
+    .then((response) => {
+      return response.json()
+    })
+    .then((json)=>{ 
+      return(
+        this.setState({
+          isLoaded: true,
+          items: json
+        })      
+      )
+    })
+  }
+  render(){
+    if(this.state.isLoaded === false){
+      return(
+        <div className="App">
+          <p>Loading...</p>
+        </div>
+      )
+    } else {
+      return(
+        <div className="App">
+          <p>CARGANDO</p>
+        </div>
+      )
+    }
+  }
+}
+export default App;
+*/
 
 class App extends Component {
   render(){
@@ -18,7 +62,6 @@ class App extends Component {
       <div className="App">
       <NavbarF />
       <Title />
-      <Contenedor />
       <ModalHungry />
       <NavbarFBottom />
       </div>
