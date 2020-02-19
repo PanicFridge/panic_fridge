bf22c4f25035cb5b8ec35e7a9a77c300e3b0bdb5
solar <jarabelmonte@gmail.com>
2020-02-19 17:43:46 +0100

    ya funciona conexión API y routes:)...parte..

--- src/components/FinalRecipe.js
+++ src/components/FinalRecipe.js
@@ -10,7 +10,7 @@ const FinalRecipe = (props) => {
             <div className="row">
                 <div className="col-12 picture">
                     <img src={props.image} alt="recipe" className="recipe_image" />
-                    <div className="col-12 recipe_title_container">
+                    <div className="recipe_title_container">
                     </div>
                     <div className="title">
                         <p>{props.title}</p>
@@ -18,16 +18,17 @@ const FinalRecipe = (props) => {
                 </div>
                 <div className="col-12 features">
                     <div className="col-4 servings">
-                        <img src="https://s4.gifyu.com/images/servings-icon.png" className="icon" alt="icono-servings" />
+                        <img src="
+https://s5.gifyu.com/images/servings-icon7efe58b90e05e545.png" className="icon" alt="icono-servings" />
                         <p>{props.servings}</p>
                     </div>
                     <div className="col-4 time">
-                        <img src="https://s4.gifyu.com/images/cooking-time-icon.png" className="icon" alt="icono-time" />
+                        <img src="https://s5.gifyu.com/images/cooking-time-icon-inverted.png" className="icon" alt="icono-time" />
                         <p>{props.readyInMinutes}</p>
                     </div>
                 </div>
                 <div className="col-12 description">
-                    <p>{props.instructions}</p>
+                    {props.instructions}
                 </div>
             </div>
         </div>
