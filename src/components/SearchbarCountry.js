25b1eec76bb6861f23f839b47661c2d824896c0b
Caterina <ccnn1010@gmail.com>
2020-02-22 02:55:21 +1100

    Changes to modal and creation of new search bar component

---
+++ src/components/SearchbarCountry.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+/*  import Country from './Country' */
+import './SearchbarCountry.scss'; 
+
+//Ponemos App como un class component porque queremos acceder al estado
+class SearchbarCountry extends Component {
+
+  state = {
+    countriesList: ['Francia', 'Chile', 'China', 'Australia', 'Guinea', 'Nigeria', 'Portugal', 'Alemania', 'Brasil', 'Andorra'],
+    inputText: ''
+  }
+
+  async changeInputText(info){
+    await this.setState({
+      inputText: info
+    })
+  }
+
+  renderCountries(){
+
+    let filteredArray = this.state.countriesList.filter((country)=>{
+      return country.toLowerCase().includes(this.state.inputText.toLowerCase())
+    })
+
+    //Hacemos un loop a los paises dentro del estado y metemos la info en Country
+    let countries = filteredArray.map((country, index)=>{
+      return (
+        <div className="blue_color">
+          <Country key={index} pais={country}/>
+        </div>
+      )
+        
+    })
+    return countries;
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <input onChange={(event)=>this.changeInputText(event.target.value)} type="text"/>
+        {this.renderCountries()}
+      </div>
+    );    
+  }
+
+  
+}
+
+export default SearchbarCountry;
