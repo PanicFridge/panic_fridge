b08eac3c86f5fffe69760c8300786b4b1dca81bf
Javiuty <javiiescuadra@gmail.com>
2020-02-24 17:40:58 +0100

    working ingredients/world-food/random

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -7,13 +7,14 @@ import FinalRecipe from './FinalRecipe'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Home from './Home';
 import Countries from './CountrySearch'
+import SearchbarCountry from './SearchbarCountry';
 
 const Routes = (props)=>{
   return(
     <div>
         <Route path='/' exact component={Home} />
         <Route path="/ingredients" component={Ingredients} />
-        <Route path="/world-food" component={Countries} />
+        <Route path="/world-food" component={SearchbarCountry} />
         <Route path="/recipe-of-the-day" component={()=> <FinalRecipe image={props.image} title={props.title} instructions={props.instructions} servings={props.servings} readyInMinutes={props.readyInMinutes}/> } />
     </div>
   )
