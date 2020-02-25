14429b0db8bea838a4e4e5ed1e62cdb32c12dc85
Javiuty <javiiescuadra@gmail.com>
2020-02-25 11:02:01 +0100

    routes fix

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -6,15 +6,14 @@ import Ingredients from './Ingredients'
 import FinalRecipe from './FinalRecipe'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Home from './Home';
-import Countries from './CountrySearch'
-import SearchbarCountry from './SearchbarCountry';
+import CountrySearch   from './CountrySearch'
 
 const Routes = (props)=>{
   return(
     <div>
         <Route path='/' exact component={Home} />
         <Route path="/ingredients" component={Ingredients} />
-        <Route path="/world-food" component={SearchbarCountry} />
+        <Route path="/world-food" component={CountrySearch} />
         <Route path="/recipe-of-the-day" component={()=> <FinalRecipe image={props.image} title={props.title} instructions={props.instructions} servings={props.servings} readyInMinutes={props.readyInMinutes}/> } />
     </div>
   )
