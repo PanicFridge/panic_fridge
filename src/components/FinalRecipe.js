040ce3848a08d01e0b1c13ade6cdf38d40cd4277
Javiuty <javiiescuadra@gmail.com>
2020-02-28 10:44:39 +0100

    re-render randomRecipes + home vh + shadow

--- src/components/FinalRecipe.js
+++ src/components/FinalRecipe.js
@@ -1,21 +1,78 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './FinalRecipe.scss';
 import Routes from './Routes';
 import NavbarF from "./NavbarF";
 import {Link} from "react-router-dom";
-const FinalRecipe = (props) => {
+class FinalRecipe extends Component {
 
-    function showProcess() {
-        return props.instructions.map((sentence, index)=>{
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    callingApiRandom (){
+        fetch("https://www.themealdb.com/api/json/v1/1/random.php", {
+          "method": "GET",
+          
+        })
+        .then((response) => {
+          let responsedInfo = response.json()
+          return responsedInfo
+        })
+        .then((dataApi)=>{
+
+    
+            let recipeTitleVar = dataApi.meals[0].strMeal;
+            let recipeImageVar = dataApi.meals[0].strMealThumb;
+            let recipiInstructionsVar = dataApi.meals[0].strInstructions;
+            let recipeCompletInforVar = dataApi.meals;
+    
+            // Función para separar las frases de las instrucciones
+            let recipeSentence = []
+            function instrSeparator(text) {
+              let sentenceArray = [];
+              sentenceArray = text.split('.');
+              recipeSentence = sentenceArray.filter((noEspaces)=>{
+                  return noEspaces !== '';
+              })
+            }
+            // Lammada a la función
+            instrSeparator(recipiInstructionsVar);
+    
+            // Cambio del estado
+            this.setState({
+              instructions: recipeSentence,
+              title: recipeTitleVar,
+              image: recipeImageVar,
+              recipeInstructions: recipiInstructionsVar,
+              info: recipeCompletInforVar
+            });
+    
+        })
+    
+        .catch (()=>{
+          return (
+            <div>
+              <h1>Upssss.... ERROR</h1>
+              <img src="https://gifyu.com/image/78Fv" alt="gif cocinando"/>
+            </div>
+          )
+        })
+      }
+
+    showProcess() {
+        return this.state.instructions.map((sentence, index)=>{
             return <li key={index}>{sentence}</li>
         })
     }
 
-    function showIngredients() {
+    showIngredients() {
         let objectKey =  [];
         let objectValue =  [];
-        for (let property in props.info[0]) {
-            objectValue.push(props.info[0][property])
+        for (let property in this.state.info[0]) {
+            objectValue.push(this.state.info[0][property])
             objectKey.push(property)
         }
         // console.log(objectKey,objectValue)
@@ -49,39 +106,54 @@ const FinalRecipe = (props) => {
 
     }
 
-    return (
-        <div className="container-fliuid FinalRecipe">
-            <div className="row">
-                <div className="col-12 picture">
-                <Link to="/"><NavbarF /></Link>
-                    <img src={props.image} alt="recipe" className="recipe_image" />
-                    <div className="recipe_title_container">
+    componentDidMount(){
+        this.callingApiRandom();
+    }
+    render(){
+        return (
+            <div className="container-fliuid FinalRecipe">
+            {
+                this.state.image == undefined
+                ? null
+                : (
+                    
+                        <div className="row">
+                        <div className="col-12 picture">
+                            <Link to="/"><NavbarF /></Link>
+                            <img src={this.state.image} alt="recipe" className="recipe_image" />
+                            <div className="recipe_title_container">
+                            </div>
+                        <div className="title">
+                            <p>{this.state.title}</p>
+                        </div>
+                        
                     </div>
-                    <div className="title">
-                        <p>{props.title}</p>
+                    <div className="col-12 features">
+                            <ol className="ingredients">
+                                    {this.showIngredients()}
+                            </ol>
+    
+                            <div className="layer">
+                            </div>
                     </div>
                     
+                            
+                    <div className="col-12 description">
+                            <ul>
+                                {this.showProcess()}
+                            </ul> 
+                    </div>
+                    
+    
                 </div>
-                <div className="col-12 features">
-                        <ol className="ingredients">
-                                {showIngredients()}
-                        </ol>
-
-                        <div className="layer">
-                        </div>
-                </div>
-                
-                        
-                <div className="col-12 description">
-                        <ul>
-                            {showProcess()}
-                        </ul> 
-                </div>
-                
-
+            
+                )
+            }
             </div>
-        </div>
-    )
+
+        )
+    }
+
 }
 
 export default FinalRecipe;
