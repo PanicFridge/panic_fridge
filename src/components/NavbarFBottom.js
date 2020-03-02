76bf9d5d7d0c59404041fc80ba3830a0c2528ce3
solar <jarabelmonte@gmail.com>
2020-03-02 12:01:46 +0100

    cambio title and icon footer

--- src/components/NavbarFBottom.js
+++ src/components/NavbarFBottom.js
@@ -10,7 +10,7 @@ const NavBottom = () => {
             <div className="container">
               <span>
               <ul className="navbar-footer">
-                <li className="col-sm-3"><Link to="/"><img className="panic-fridge-icon"src="https://s4.gifyu.com/images/fridge-link-inverted-01.png" alt="panic fridge icon"/></Link></li>
+                <li className="col-sm-3"><Link to="/"><img className="panic-fridge-icon"src="https://s5.gifyu.com/images/fridge-icon-turquoise-footer-05.png" alt="panic fridge icon"/></Link></li>
                 <li className="col-sm-3"><Link to="/ingredients"><img className="ingredients-icon" src="https://s5.gifyu.com/images/ingredients-green-TEXT-02-02.png" alt="ingredients icon" /></Link></li>
                 <li className="col-sm-3"><Link to="/World-food"><img className="world-icon" src="https://s5.gifyu.com/images/world-food-inverted-TEXT-03ede060ca68ea843e.png" alt="World icon"/></Link></li>
                 <li className="col-sm-3"><Link to="/recipe-of-the-day"><img className="suggestion-icon"src="https://s5.gifyu.com/images/random-recipe-01.png" alt="Recipe of the day icon"/></Link></li>
