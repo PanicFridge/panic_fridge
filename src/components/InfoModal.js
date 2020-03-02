cc406c42a6afbeae8bfc8df0be1b6c0c21109ed0
Javiuty <javiiescuadra@gmail.com>
2020-03-02 15:07:48 +0100

    conection routes of modalIfo

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -35,21 +35,21 @@ class InfoModal extends Component {
           </div>
           <div className="container-mother">
             <div className="container-rrss1">
-              <a href="https://github.com/ccnn1010"><img className="github-icon1" src="https://s5.gifyu.com/images/github.jpg" alt="github-icon" width="66" height="40" /></a>
-              <a href="https://www.linkedin.com/in/caterinanicolo/"><img className="linkedin-icon1" src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon" width="50" height="45" /></a>
+              <a href="https://github.com/ccnn1010" target="_blank" rel="noopener noreferrer"><img className="github-icon1" src="https://s5.gifyu.com/images/github.jpg" alt="github-icon" width="66" height="40"/></a>
+              <a href="https://www.linkedin.com/in/caterinanicolo/" target="_blank" rel="noopener noreferrer"><img className="linkedin-icon1" src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon" width="50" height="45" /></a>
 
             
-              <a href="https://github.com/jciria74"><img className="github-icon2" src="https://s5.gifyu.com/images/github.jpg" alt="github-icon" width="66" height="40" /></a>
-              <a href="https://www.linkedin.com/in/javierciria74/"><img className="linkedin-icon2" src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon" width="50" height="45" /></a>
+              <a href="https://github.com/jciria74" target="_blank" rel="noopener noreferrer"><img className="github-icon2" src="https://s5.gifyu.com/images/github.jpg" alt="github-icon" width="66" height="40" /></a>
+              <a href="https://www.linkedin.com/in/javierciria74/" target="_blank" rel="noopener noreferrer"><img className="linkedin-icon2" src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon" width="50" height="45" /></a>
            
           
             
-              <a href="https://github.com/jara-git"><img className="github-icon3" src="https://s5.gifyu.com/images/github.jpg" alt="github-icon" width="66" height="40" /></a>
-              <a href="https://www.linkedin.com/in/jarabelmonte/"><img className="linkedin-icon3" src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon" width="50" height="45" /></a>
+              <a href="https://github.com/jara-git" target="_blank" rel="noopener noreferrer"><img className="github-icon3" src="https://s5.gifyu.com/images/github.jpg" alt="github-icon" width="66" height="40" /></a>
+              <a href="https://www.linkedin.com/in/jarabelmonte/" target="_blank" rel="noopener noreferrer"><img className="linkedin-icon3" src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon" width="50" height="45" /></a>
          
     
-              <a href="https://github.com/Javiuty"><img className="github-icon4" src="https://s5.gifyu.com/images/github.jpg" alt="github-icon" width="66" height="40" /></a>
-              <a href="https://www.linkedin.com/in/javierescuadra/"><img className="linkedin-icon4" src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon" width="50" height="45" /></a>
+              <a href="https://github.com/Javiuty" target="_blank" rel="noopener noreferrer"><img className="github-icon4" src="https://s5.gifyu.com/images/github.jpg" alt="github-icon" width="66" height="40" /></a>
+              <a href="https://www.linkedin.com/in/javierescuadra/" target="_blank" rel="noopener noreferrer"><img className="linkedin-icon4" src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon" width="50" height="45" /></a>
             </div>
           </div>
           </ModalBody>
