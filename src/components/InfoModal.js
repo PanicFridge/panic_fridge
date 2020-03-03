9fef9d2cbf5c521f44eb5b1b61007523568597ed
Javiuty <javiiescuadra@gmail.com>
2020-03-03 10:19:55 +0100

    navbar + modal

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -27,27 +27,28 @@ class InfoModal extends Component {
            <p>Who we are?</p>
           </ModalHeader>
           <ModalBody>
+          {/* -----------------------Nuestras fotos --------------------------*/}
           <div className="container-icon">
             <img  src="https://s5.gifyu.com/images/cat36b02dbb58a70018.png" alt="insert" width="100" height="90" />
             <img src="https://s5.gifyu.com/images/javi.png" alt="insert" width="100" height="90" />
             <img src="https://s5.gifyu.com/images/Jara.png" alt="insert" width="100" height="90" />
             <img src="https://s5.gifyu.com/images/Escu.png" alt="insert" width="100" height="90" />
           </div>
+          {/* -------------------------Iconos de Github y Linkedin------------- */}
           <div className="container-mother">
             <div className="container-rrss1">
+              {/* ----------Caterina--------- */}
               <a href="https://github.com/ccnn1010" target="_blank" rel="noopener noreferrer"><img className="github-icon1" src="https://s5.gifyu.com/images/github.jpg" alt="github-icon" width="66" height="40"/></a>
               <a href="https://www.linkedin.com/in/caterinanicolo/" target="_blank" rel="noopener noreferrer"><img className="linkedin-icon1" src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon" width="50" height="45" /></a>
 
-            
+              {/* ---------Javi-------------- */}
               <a href="https://github.com/jciria74" target="_blank" rel="noopener noreferrer"><img className="github-icon2" src="https://s5.gifyu.com/images/github.jpg" alt="github-icon" width="66" height="40" /></a>
               <a href="https://www.linkedin.com/in/javierciria74/" target="_blank" rel="noopener noreferrer"><img className="linkedin-icon2" src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon" width="50" height="45" /></a>
            
-          
-            
+              {/* --------------Jara------------------------ */}
               <a href="https://github.com/jara-git" target="_blank" rel="noopener noreferrer"><img className="github-icon3" src="https://s5.gifyu.com/images/github.jpg" alt="github-icon" width="66" height="40" /></a>
               <a href="https://www.linkedin.com/in/jarabelmonte/" target="_blank" rel="noopener noreferrer"><img className="linkedin-icon3" src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon" width="50" height="45" /></a>
-         
-    
+              {/* -------------Escu-------------------------- */}
               <a href="https://github.com/Javiuty" target="_blank" rel="noopener noreferrer"><img className="github-icon4" src="https://s5.gifyu.com/images/github.jpg" alt="github-icon" width="66" height="40" /></a>
               <a href="https://www.linkedin.com/in/javierescuadra/" target="_blank" rel="noopener noreferrer"><img className="linkedin-icon4" src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon" width="50" height="45" /></a>
             </div>
--- src/components/NavbarF.js
+++ src/components/NavbarF.js
@@ -6,20 +6,7 @@ const Nav = () => {
       return (
 
       <div className='App nav-fridge'>
-        <nav className="navbar navbar-expand-md navbar-expand-lg navbar-expand-xl nav-fridge">
           <div className= 'fridge-logo'></div>
-            {/* <link className="navbar-brand" href="/"></link> */}
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                  <div className="navbar-nav">
-                      {/* <link className="nav-item nav-link active" href="/ingredients">INGREDIENTS <span className="sr-only">(current)</span></link>
-                      <link className="nav-item nav-link" href="/world-food">WORLD FOOD</link>
-                      <link className="nav-item nav-link" href="/recipe-of-the-day">RECIPE OF THE DAY</link> */}
-                  </div>
-              </div>  
-        </nav>
       </div>
 )
 }
