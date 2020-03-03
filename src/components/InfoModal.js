bb354ac5a6cc6d07a10718cac420fdf93e1a70db
jciria74 <47560520+jciria74@users.noreply.github.com>
2020-03-03 22:44:14 +0100

    ajustes de modal para movil

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -37,7 +37,61 @@ class InfoModal extends Component {
           </ModalHeader>
           <ModalBody>
           {/* -----------------------Nuestras fotos --------------------------*/}
-            <div className="faces-icons">
+            <div className="container">
+              <div className="row">
+                <div className="faces-icons col-3">
+                  <img  src="https://s5.gifyu.com/images/cat36b02dbb58a70018.png" alt="insert"/>
+                </div>
+                <div className="text-name col-7">
+                  <h5>Caterina Nicolo</h5>
+                  <p>ccnn1010@gmail.com</p>
+                </div>
+                <div className="icons col-2">
+                  <a href="https://github.com/ccnn1010" target="_blank" rel="noopener noreferrer"><img className="github-icon" src="https://s5.gifyu.com/images/github-icon.png" alt="github-icon"/></a>
+                  <a href="https://www.linkedin.com/in/caterinanicolo/" target="_blank" rel="noopener noreferrer"><img className="linkedin-icon" src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon" /></a>
+                </div>
+
+                <div className="faces-icons col-3">
+                  <img  src="https://s5.gifyu.com/images/javi.png" alt="insert"/>
+                </div>
+                <div className="text-name col-7">
+                  <h5>Javier Sánchez</h5>
+                  <p>jciria74@gmail.com</p>
+                </div>
+                <div className="icons col-2">
+                  <a href="https://github.com/jciria74" target="_blank" rel="noopener noreferrer"><img className="github-icon" src="https://s5.gifyu.com/images/github-icon.png" alt="github-icon" /></a>
+                  <a href="https://www.linkedin.com/in/javierciria74/" target="_blank" rel="noopener noreferrer"><img className="linkedin-icon" src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon"/></a>
+                </div>
+
+                <div className="faces-icons col-3">
+                  <img  src="https://s5.gifyu.com/images/Jara.png" alt="insert"/>
+                </div>
+                <div className="text-name col-7">
+                  <h5>Jara Belmonte</h5>
+                  <p>jarabelmonte@gmail.com</p>
+                </div>
+                <div className="icons col-2">
+                  <a href="https://github.com/jara-git" target="_blank" rel="noopener noreferrer"><img className="github-icon"  src="https://s5.gifyu.com/images/github-icon.png" alt="github-icon" /></a>
+                  <a href="https://www.linkedin.com/in/jarabelmonte/" target="_blank" rel="noopener noreferrer"><img className="linkedin-icon"src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon"/></a>
+                </div>
+
+                <div className="faces-icons col-3">
+                  <img  src="https://s5.gifyu.com/images/Escu.png" alt="insert"/>
+                </div>
+                <div className="text-name col-7">
+                  <h5>Javier Escuadra</h5>
+                  <p>javiiescuadra@gmail.com</p>
+                </div>
+                <div className="icons col-2">
+                  <a href="https://github.com/Javiuty" target="_blank" rel="noopener noreferrer"><img className="github-icon" src="https://s5.gifyu.com/images/github-icon.png" alt="github-icon" /></a>
+                  <a href="https://www.linkedin.com/in/javierescuadra/" target="_blank" rel="noopener noreferrer"><img className="linkedin-icon" src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon"/></a>
+                </div>
+
+              </div>
+            </div>
+
+
+            {/* <div className="faces-icons">
               <img  src="https://s5.gifyu.com/images/cat36b02dbb58a70018.png" alt="insert"/>
               <img src="https://s5.gifyu.com/images/javi.png" alt="insert"  />
               <img src="https://s5.gifyu.com/images/Jara.png" alt="insert"  />
@@ -73,7 +127,7 @@ class InfoModal extends Component {
                 <a href="https://www.linkedin.com/in/javierescuadra/" target="_blank" rel="noopener noreferrer"><img src="https://s5.gifyu.com/images/linkedin80f6790467704d21.png" alt="linkedin-icon"/></a>
               </div>
               
-            </div>
+            </div> */}
     
           </ModalBody>
           <ModalFooter id="modal-footer" >
